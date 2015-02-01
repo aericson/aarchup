baada30e2fcad148e5e83e603043fd01231a330e
André Ericson <de.ericson@gmail.com>
2015-02-01 15:34:20 -0300

    Remove dependency of gconf-helper from noxaarchup script

--- src/systemd_files/noxaarchup.sh
+++ src/systemd_files/noxaarchup.sh
@@ -2,13 +2,22 @@
 
 # based on script from https://aur.archlinux.org/packages/batterylife
 call_aarchup(){
+    dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS $1 | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+    user=$(grep -m 1 -z USER $1 | sed 's/USER=//')
+    dply=$(grep -z DISPLAY $1 | sed 's/DISPLAY=//')
+    sudo -u $user sh -c "DBUS_SESSION_BUS_ADDRESS=\"$dbus\" DISPLAY=\"$dply\" /usr/bin/aarchup"   
+}
+
+wrapper(){
+    called=false
     for p in $(pgrep gconf-helper); do
-        dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$p/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
-        user=$(grep -m 1 -z USER /proc/$p/environ | sed 's/USER=//')
-        dply=$(grep -z DISPLAY /proc/$p/environ | sed 's/DISPLAY=//')
-        sudo -u $user sh -c "DBUS_SESSION_BUS_ADDRESS=\"$dbus\" DISPLAY=\"$dply\" /usr/bin/aarchup"
+        call_aarchup /proc/$p/environ
+        called=true
     done
+    if [ $called = false ]; then
+        call_aarchup /proc/self/environ
+    fi  
 }
 
 pacman -Sy || exit 1
-call_aarchup || exit 1
+wrapper || exit 1
